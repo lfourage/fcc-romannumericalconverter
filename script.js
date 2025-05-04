6ba947777f34f41950dcd7875be412db88e16ef4
lfourage <ludogriph@gmail.com>
2025-05-04 17:01:06 +0200

    cleaned code presentation

--- script.js
+++ script.js
@@ -3,58 +3,19 @@ const convertBtn = document.getElementById("convert-btn");
 const output = document.getElementById("output");
 
 const convertTable = [
-    {
-        arabic: 1000,
-        roman: "M",
-    },
-    {
-        arabic: 900,
-        roman: "CM",
-    },
-    {
-        arabic: 500,
-        roman: "D",
-    },
-    {
-        arabic: 400,
-        roman: "CD",
-    },
-    {
-        arabic: 100,
-        roman: "C",
-    },
-    {
-        arabic: 90,
-        roman: "XC",
-    },
-    {
-        arabic: 50,
-        roman: "L",
-    },
-    {
-        arabic: 40,
-        roman: "XL",
-    },
-    {
-        arabic: 10,
-        roman: "X",
-    },
-    {
-        arabic: 9,
-        roman: "IX",
-    },
-    {
-        arabic: 5,
-        roman: "V",
-    },
-    {
-        arabic: 4,
-        roman: "IV",
-    },
-    {
-        arabic: 1,
-        roman: "I",
-    }
+    { arabic: 1000, roman: "M" },
+    { arabic: 900, roman: "CM" },
+    { arabic: 500, roman: "D" },
+    { arabic: 400, roman: "CD" },
+    { arabic: 100, roman: "C" },
+    { arabic: 90, roman: "XC" },
+    { arabic: 50, roman: "L" },
+    { arabic: 40, roman: "XL" },
+    { arabic: 10, roman: "X" },
+    { arabic: 9, roman: "IX" },
+    { arabic: 5, roman: "V" },
+    { arabic: 4, roman: "IV" },
+    { arabic: 1, roman: "I" }
 ]
 
 const parsing = () => {
@@ -76,22 +37,19 @@ const parsing = () => {
     return number;
 }
 
-const convertNumber = (number) => {
+const decimalToRoman = (number) => {
     const convertEl = convertTable.find((el) => number - el.arabic >= 0);
 
     if (convertEl) {
         output.innerText += convertEl.roman;
-        convertNumber(number - convertEl.arabic);
+        decimalToRoman(number - convertEl.arabic);
     }
-    return;
 }
 
-const decimalToRoman = () => {
+const converter = () => {
     const number = parsing();
 
-    if (!number)
-        return;
-    convertNumber(number);
+    decimalToRoman(number);
 }
 
-convertBtn.addEventListener("click", decimalToRoman);
+convertBtn.addEventListener("click", converter);
