0797107d0ada4e3a141e84dea6522501512df372
lfourage <ludogriph@gmail.com>
2025-05-04 14:00:49 +0200

    getElements + event listener + empty function

---
+++ script.js
@@ -0,0 +1,9 @@
+const numnerInput = document.getElementById("number-input");
+const convertBtn = document.getElementById("convert-btn");
+const output = document.getElementById("output");
+
+const decimalToRoman = () => {
+    return ;
+}
+
+convertBtn.addEventListener("click", decimalToRoman);
